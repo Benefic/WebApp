8d94fb92fc2ce93def59b8bdb00cf1907b21ab28
ALEKSANDR KOYNOSOV <abenefic@gmail.com>
2021-03-15 19:17:16 +0300

    lesson 10 spec & swagger

--- src/main/java/ru/abenefic/spring/controller/ProductController.java
+++ src/main/java/ru/abenefic/spring/controller/ProductController.java
@@ -3,13 +3,17 @@ package ru.abenefic.spring.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import ru.abenefic.spring.exceptions.NoSuchPageException;
 import ru.abenefic.spring.model.ProductSort;
 import ru.abenefic.spring.model.SortDirection;
 import ru.abenefic.spring.model.dtos.ProductDto;
+import ru.abenefic.spring.model.entities.Product;
+import ru.abenefic.spring.repository.specifications.ProductSpecifications;
 import ru.abenefic.spring.services.ProductService;
 
 import java.util.List;
@@ -26,15 +30,11 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductDto>> getAll(@RequestParam(name = "s", defaultValue = "") String search,
-                                                   @RequestParam(defaultValue = "") String title,
-                                                   @RequestParam(defaultValue = "0") float first,
-                                                   @RequestParam(defaultValue = "0") float second,
-                                                   @RequestParam(defaultValue = "0") float cost,
+    public ResponseEntity<List<ProductDto>> getAll(@RequestParam MultiValueMap<String, String> params,
                                                    @RequestParam(defaultValue = "1") int page,
                                                    @RequestParam(defaultValue = "10") int size,
-                                                   @RequestParam(defaultValue = "ASC") SortDirection costSort,
-                                                   @RequestParam(defaultValue = "ASC") SortDirection titleSort,
+                                                   @RequestParam(defaultValue = "ASC") SortDirection costSortDirection,
+                                                   @RequestParam(defaultValue = "ASC") SortDirection titleSortDirection,
                                                    @RequestParam(defaultValue = "TITLE") ProductSort mainSort
     ) {
 
@@ -44,8 +44,8 @@ public class ProductController {
             page--;
         }
 
-        Sort.Order costSorting = new Sort.Order(Sort.Direction.valueOf(costSort.name()), "cost");
-        Sort.Order titleSorting = new Sort.Order(Sort.Direction.valueOf(titleSort.name()), "title");
+        Sort.Order costSorting = new Sort.Order(Sort.Direction.valueOf(costSortDirection.name()), "cost");
+        Sort.Order titleSorting = new Sort.Order(Sort.Direction.valueOf(titleSortDirection.name()), "title");
 
         Sort resultSort;
         if (mainSort == ProductSort.COST) {
@@ -54,15 +54,10 @@ public class ProductController {
             resultSort = Sort.by(titleSorting, costSorting);
         }
 
-        Page<ProductDto> products;
+        Specification<Product> spec = ProductSpecifications.build(params);
 
-        switch (search) {
-            case "title_like" -> products = productService.getAllByTitleContains(title, page, size, resultSort);
-            case "cost_between" -> products = productService.getAllByCostBetween(first, second, page, size, resultSort);
-            case "cost_less" -> products = productService.getAllByCostIsLessThanEqual(cost, page, size, resultSort);
-            case "cost_greater" -> products = productService.getAllByCostGreaterThanEqual(cost, page, size, resultSort);
-            default -> products = productService.getAll(page, size, resultSort);
-        }
+        Page<ProductDto> products = productService.getAll(spec, page, size, resultSort);
+
         if (products.getTotalPages() <= page) {
             throw new NoSuchPageException("Maximum page is " + products.getTotalPages());
         }
--- src/main/java/ru/abenefic/spring/controller/ShoppingCartController.java
+++ src/main/java/ru/abenefic/spring/controller/ShoppingCartController.java
@@ -2,14 +2,13 @@ package ru.abenefic.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.abenefic.spring.model.dtos.ProductDto;
 import ru.abenefic.spring.services.ShoppingCartService;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/cart")
 public class ShoppingCartController {
 
