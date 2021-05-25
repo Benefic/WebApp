1ae72bce1eba0715410b248f7e94d00998e76cdf
Benefic <abenefic@gmail.com>
2021-05-25 23:47:11 +0300

    Eureka server

---
+++ eureka-server/src/test/java/ru/abenefic/spring/shop/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.abenefic.spring.shop.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
